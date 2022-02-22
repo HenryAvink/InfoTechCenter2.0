4ef3c9591899f105b95e60cd79465edd5d2d9885
Henry Avink <henryscomputerscience@gmail.com>
2022-02-22 13:42:33 -0500

    Changed Int in miles Variable to a Float

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -12,18 +12,17 @@ def gasLevelGauge():
 # Variable calls the value of gasLevelGauge Function
 gasLevelIndicator = gasLevelGauge()
 
-
 # Create If-ElIf-Else statements using the Comparative Operator == (Equal to) to display gas level messages
 def gasLevelAlert():
     gasStations = ["Shell", "BP", "Citgo", "Circle K", "Mobil", "Speedway", "Marathon", "Love's", "Meijer", "Costco", "Sunoco"]
-    miles = random.randint(1,25)
+    miles = round(random.uniform(1.0,25.0), 2)
     gasLevelIndicator = gasLevelGauge()
     if gasLevelIndicator == "Empty":
         print("! ! !WARNING - YOU ARE ON EMPTY! ! !\nCalling Emergency Contact")
     elif gasLevelIndicator == "Low":
-        print("! ! !WARNING - LOW GASOLINE! ! !\nThe closest gas station is " + random.choice(gasStations) + ". Distance is " + str(miles) + " miles away")
+        print("! ! !WARNING - LOW GASOLINE! ! !\nThe closest gas station is " + random.choice(gasStations) + " which is " + str(miles) + " miles away")
     elif gasLevelIndicator == "Quarter Tank":
-        print("You are on a QUARTER TANK of gas\nStart planning a visit to the gas station")
+        print("You are on a QUARTER TANK of gas\nStart planning a visit to the gas station. Closest gas station is " + random.choice(gasStations) + " which is " + str(miles) + " miles away")
     elif gasLevelIndicator == "Half Tank":
         print("You are on a HALF TANK of gas\nYou have 125 miles to empty")
     elif gasLevelIndicator == "Three Quarter Tank":
