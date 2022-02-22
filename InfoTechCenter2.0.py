b46c70142dce7bfe76a69356c7c0ce3751914f0d
Henry Avink <henryscomputerscience@gmail.com>
2022-02-22 13:38:34 -0500

    Initial gasLevelAlert w/If statement stable

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -6,6 +6,15 @@ import random
 def gasLevelGauge():
     gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full"]
     currentGasLevel = random.choice(gasLevelList)
-    print(currentGasLevel)
+    # print(currentGasLevel)
+    return currentGasLevel
 
-gasLevelGauge()
+# Create If-ElIf-Else statements using Comparative Operators to display gas level messages
+def gasLevelAlert():
+    if gasLevelGauge() == "Empty":
+        print("! ! !WARNING! ! !\nYou have run out of gas\nCalling Emergency Contact")
+
+
+
+
+gasLevelAlert()
