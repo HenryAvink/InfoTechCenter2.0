4daf0597c876ee67b8ba31d1f4237df788a4913f
Henry Avink <henryscomputerscience@gmail.com>
2022-02-22 13:29:12 -0500

    Imported Colorama and added color to print statements

--- InfoTechCenter2.0.py
+++ InfoTechCenter2.0.py
@@ -1,7 +1,10 @@
 # Code Name - Hornet
 
 from time import sleep #Print to one line with time deley between prints
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(strip=False, autoreset=True)
 
-print("Welcome to Hornet's InfoTechCenter\n")
+print(Fore.RED + "Welcome to Hornet's InfoTechCenter\n")
 sleep(1)
-print("Hornet's Operating System Booting Up")
+print(Fore.RED + "Hornet's Operating System Booting Up")
